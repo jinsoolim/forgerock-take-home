ff7106cb74a3ce2708b553914b7937e0ed69c31f
john lim <johnlim@johns-MacBook-Pro.local>
2021-06-27 18:11:16 -0700

    Minor change

--- src/OfficePage/Footer/Footer.tsx
+++ src/OfficePage/Footer/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer(): JSX.Element {
   return (
     <div id="footer">
       <p>
-        This page is for The Office fans! Hover or click on a character bubble to find out more information about your favorite characters!
+        This page is for The Office fans! <u>Hover</u> or <u>click</u> on a character bubble to find out more information about your favorite characters!
       </p>
     </div>
   );
